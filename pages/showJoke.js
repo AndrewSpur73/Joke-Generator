acc5727f4c097e34691c842df1a9a0e2d91271b0
Andrew <andrewspurlock@rocketmail.com>
2024-02-18 19:23:56 -0600

    project completed

---
+++ pages/showJoke.js
@@ -0,0 +1,20 @@
+import renderToDom from '../utils/renderToDom';
+
+const showJoke = (joke) => {
+  let domString = '';
+  domString += `<h3>${joke.setup}</h3>`;
+
+  renderToDom('#joke', domString);
+  return joke;
+};
+
+const showPunchline = (joke) => {
+  console.warn(joke, 'show Punchline');
+
+  let domString = '';
+  domString += `<h3>${joke.delivery}</h3>`;
+
+  renderToDom('#punchline', domString);
+};
+
+export { showJoke, showPunchline };
